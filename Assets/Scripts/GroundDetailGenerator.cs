f0f4d52227f561063403d03b0d74b4654f33e8d3
Slickgotchi <slickgotchi@gmail.com>
2025-02-19 10:06:52 +0700

    ground detail tilemap generation done

--- Assets/Scripts/GroundDetailGenerator.cs
+++ Assets/Scripts/GroundDetailGenerator.cs
@@ -11,6 +11,9 @@ public class GroundDetailGenerator : MonoBehaviour
     public Tilemap groundDetailA_Tilemap;
     public RuleTile groundDetailA_RuleTile;
 
+    public Tilemap groundDetailB_Tilemap;
+    public RuleTile groundDetailB_RuleTile;
+
     private void Awake()
     {
         if (Instance == null)
@@ -27,6 +30,7 @@ public class GroundDetailGenerator : MonoBehaviour
     public void Generate()
     {
         groundDetailA_Tilemap.ClearAllTiles();
+        groundDetailB_Tilemap.ClearAllTiles();
 
         // iterate over rooms and draw in using snoise
         var rooms = LevelGenerator.Instance.GetRooms();
@@ -36,80 +40,40 @@ public class GroundDetailGenerator : MonoBehaviour
             {
                 for (int y = room.yMin; y < room.yMax-1; y++)
                 {
-                    var noiseValue = noise.snoise(new float2(x, y));
-                    if (noiseValue > 0.618f)
+                    var noiseValue = (noise.snoise(new float2(x, y)) + 1) * 0.5f;
+                    if (noiseValue > 0.87f)
                     {
-                        //groundDetailA_Tilemap.SetTile(new Vector3Int(x, y, 0), groundDetailA_RuleTile);
-                        Set3x2TileRectangle(x, y);
+                        SetTileRectangle(x, y, 6, 4, true);
+                    }
+
+                    noiseValue = (noise.snoise(new float2(x+10, y+10)) + 1) * 0.5f;
+                    if (noiseValue > 0.86f)
+                    {
+                        SetTileRectangle(x, y, 3, 2, false);
                     }
                 }
             }
         }
-
-        RemoveSingleTiles();
     }
 
-    void Set3x2TileRectangle(int x, int y)
+    void SetTileRectangle(int x, int y, int width, int height, bool isA)
     {
-        for (int i = x; i < x + 3; i++)
+        for (int i = x; i < x + width; i++)
         {
-            for (int j = y; j < y + 2; j++)
+            for (int j = y; j < y + height; j++)
             {
                 if (LevelGenerator.Instance.IsInRoomBounds(i, j))
                 {
-                    Debug.Log("In room bounds");
-                    groundDetailA_Tilemap.SetTile(new Vector3Int(i, j, 0), groundDetailA_RuleTile);
+                    if (isA)
+                    {
+                        groundDetailA_Tilemap.SetTile(new Vector3Int(i, j, 0), groundDetailA_RuleTile);
+                    }
+                    else
+                    {
+                        groundDetailB_Tilemap.SetTile(new Vector3Int(i, j, 0), groundDetailB_RuleTile);
+                    }
                 }
             }
         }
     }
-
-
-    void RemoveSingleTiles()
-    {
-        var tilemapBounds = groundDetailA_Tilemap.cellBounds;
-        for (int x = tilemapBounds.xMin; x < tilemapBounds.xMax-1; x++)
-        {
-            for (int y = tilemapBounds.yMin; y < tilemapBounds.yMax-1; y++)
-            {
-
-            }
-        }
-    }
-
-    bool IsTileAdjacent(int x, int y, AdjacentSide adjacentSide)
-    {
-        Vector3Int checkVector = GetAdjacentSideVector3Int(x, y, adjacentSide);
-        return groundDetailA_Tilemap.GetTile(checkVector) as RuleTile == groundDetailA_RuleTile;
-    }
-
-    Vector3Int GetAdjacentSideVector3Int(int x, int y, AdjacentSide adjacentSide)
-    {
-        switch (adjacentSide)
-        {
-            case AdjacentSide.TopLeft: return new Vector3Int(x - 1, y + 1, 0);
-            case AdjacentSide.TopMiddle: return new Vector3Int(x, y + 1, 0);
-            case AdjacentSide.TopRight: return new Vector3Int(x + 1, y + 1, 0);
-
-            case AdjacentSide.MiddleLeft: return new Vector3Int(x - 1, y, 0);
-            case AdjacentSide.MiddleRight: return new Vector3Int(x + 1, y, 0);
-
-            case AdjacentSide.BottomLeft: return new Vector3Int(x - 1, y - 1, 0);
-            case AdjacentSide.BottomMiddle: return new Vector3Int(x, y - 1, 0);
-            case AdjacentSide.BottomRight: return new Vector3Int(x + 1, y - 1, 0);
-
-            default: break;
-        }
-
-        return Vector3Int.zero;
-    }
-
-    public enum AdjacentSide
-    {
-        TopLeft, TopMiddle, TopRight,
-        MiddleLeft, MiddleRight,
-        BottomLeft, BottomMiddle, BottomRight
-    }
-
-
 }
